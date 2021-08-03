24ba2f1e69b52a652b597a58d8a441a54d253720
John Yasis <johnamielyasis@gmail.com>
2021-08-02 20:04:08 -0700

    finish header, add parallax effect

--- src/components/header.js
+++ src/components/header.js
@@ -13,10 +13,10 @@ export default function Header() {
 
   return (
     <>
-      <nav className="relative flex lg:flex-row items-center justify-between px-2 py-3 mb-3 align-top"
+      <nav className="relative flex lg:flex-row items-center justify-between px-2 py-3 mb-5 align-top"
         role="navigation"
       >
-        <div className="flex items-center bg-white lg:flex-row rounded-xl h-12">
+        <div className="flex items-center bg-white lg:flex-row rounded-xl h-12 shadow-md">
           <div className="flex relative px-3 pr-4 whitespace-nowrap hover:bg-yellow-300 hover:text-black">
             <Link to="/">
               <svg className="inline pb-1 w-6 h-6 text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12 14l9-5-9-5-9 5 9 5z" /><path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" /><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" /></svg>
@@ -43,12 +43,23 @@ export default function Header() {
           </Link>
         </div>
       </nav>
-      <div className="grid grid-cols-2">
-        <div className="bg-blue-500 grid justify-items-center text-4xl">
-          <h1>Student Union <br /> Hour</h1>
+      <div className="grid grid-cols-2 mt-12 pt-12">
+        <div className="grid justify-items-center pt-2 pl-5">
+          <h1 className="font-extrabold drop-shadow-md">Student <br />
+              Union <br />
+              Hour
+          </h1>
         </div>
-        <div className="bg-red-500 ">
-          <p>This is where the card will go</p>
+        <div className="grid justify-items-center pt-2 pl-12">
+          <p>
+            How's it feel to be <br />
+            at the center of magic <br />
+            to linger in tones <br />
+            and words?
+          </p>
+          <button className="bg-yellow-400 text-white rounded-xl px-5 py-2 font-semibold shadow-md">
+            Contact Us!
+          </button>
         </div>
       </div>
     </>
--- src/components/layout.js
+++ src/components/layout.js
@@ -24,15 +24,15 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <div className="h-screen object-fill min-h-full w-full bg-cover font-sans" style={{backgroundImage: `url(https://images.unsplash.com/photo-1504275107627-0c2ba7a43dba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80)`}} >
-      <Header  siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <div className="h-screen object-fill min-h-full w-full bg-cover font-sans bg-fixed" style={{ backgroundImage: `url(https://images.unsplash.com/photo-1504275107627-0c2ba7a43dba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80)` }} >
+        <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       </div>
-        <main>
-          <p>this is body</p>
-        </main>
-        <footer>
-          <p>this is footer</p>
-        </footer>
+      <main>
+        <p>this is body</p>
+      </main>
+      <footer>
+        <p>this is footer</p>
+      </footer>
     </>
   )
 }
