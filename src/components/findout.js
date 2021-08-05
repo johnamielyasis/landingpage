c3291e32895de8b9130b83bca21200af82c2ecfb
John Yasis <johnamielyasis@gmail.com>
2021-08-05 01:38:15 -0700

    merge

--- src/components/findout.js
+++ src/components/findout.js
@@ -6,7 +6,7 @@ export default function FindOutMore({ path }) {
     <div>
       <Link to={`/${path}`}>
         <span className="flex">
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" /></svg>
+          <svg className="w-6 h-6" alt="arrow pointing right" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" /></svg>
           <span className="font-extrabold px-3">
             Find out More
           </span>
--- src/components/settings.js
+++ src/components/settings.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { useRecoilValue } from 'recoil';
+import { settingsAtom } from '../atoms';
 
 export default function Settings() {
+  const { primaryColor } = useRecoilValue(settingsAtom);
+
   return (
     <div className="flex">
       <svg className="w-6 h-6 text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" /></svg>
-      <a href="#Settings" className="font-extrabold text-black hover:text-yellow-400 px-3">
+      <a href="#Settings" className={`font-extrabold text-black hover:text-${primaryColor}-400 px-3`}>
         Edit Me!
       </a>
     </div>
